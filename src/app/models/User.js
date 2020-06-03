dfe875fd30fdaf5115be7740df1619cfd268614f
Vinicius de Carvalho Barboza <vini_000@live.com>
2020-06-03 14:17:55 -0300

    Upload de arquivos com multer

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -7,8 +7,8 @@ class User extends Model {
       {
         name: Sequelize.STRING,
         email: Sequelize.STRING,
-        password: Sequelize.VIRTUAL,
         password_hash: Sequelize.STRING,
+        password: Sequelize.VIRTUAL,
         provider: Sequelize.BOOLEAN,
       },
       { sequelize }
--- src/routes.js
+++ src/routes.js
@@ -2,13 +2,19 @@ import { Router } from 'express';
 
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
-import authMiddleware from './app/middlewares/auth'
+import authMiddleware from './app/middlewares/auth';
+import multer from 'multer';
+import multerConfig from './config/multer';
 
+const upload = multer(multerConfig);
 const routes = new Router();
 
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
 routes.use(authMiddleware);
 routes.put('/users', UserController.update);
+routes.post('/files',upload.single('file'), (req, res) =>{
+    return res.json({ ok: true });
+})
 
 export default routes;
